406a32df263b145cbf9c8e88e98ed406d7cde566
lauryngrootfaam <grootfaamlauryn@gmail.com>
2022-09-15 13:16:44 +0200

    some code to practice with js

--- index.js
+++ index.js
@@ -1,36 +1,35 @@
-
-
 const nav = document.querySelectorAll("a");
-let inputGebruiker= "Sale";
+let inputGebruiker = "Sale";
 
 for (let navItem of nav) {
 
-    if (navItem.textContent === inputGebruiker){
-                console.log("er is een link gelijk aan sale");
-                navItem.className += " redText"; 
-           }
-           
-           else {
-                console.log("er gebeurd niets");
-           }
-        
+    if (navItem.textContent === inputGebruiker) {
+        console.log("Er is een element, " + inputGebruiker + ", in de navigatie gelijk aan de input.");
+        navItem.className += " redText";
+    } else {
+        console.log("Het navigatie-item is niet gelijk aan de input van de gebruiker");
+    }
 }
 
 
+// Als een gebruiker met de muis over bepaalde navigatie items gaat (hover/mousover), dan bij bepaalde waarde verschijnt er submenu met categorieën
+//misschien checken of een navigatieitem een sub-catgerieen heeft, zo ja laat ze zien, zo nee gebeurd er niets
+// Bij de menuwaarde footwear, kleding, accesoires, lifestyle, releases, brands
 
-// const navLink = document.getElementById("test");
+
+//bij mousover/hover event laat vlak zien // als iemand hovert over footwear, add class display none
+
+const footwearItem = document.querySelector(".test");
+const subMenu = document.getElementsByClassName("submenu");
 
 
-// function makeTextRed(navItem){
-//     if (navItem === "Sale"){
-//         console.log("er is een link gelijk aan sale");
-//         navLink.className += " redText"; 
-//    }
-   
-//    else {
-//         console.log("er gebeurd niets");
-//    }
+function showSubMenu() {
+    console.log("de functie wordt uitgevoerd bij een mouse over");
+    
+    // add and delete class display
 
-// }
+}
 
-// makeTextRed(navLink.textContent);
+showSubMenu();
+
+footwearItem.addEventListener('mouseover', showSubMenu);
---
+++ klad.js
@@ -0,0 +1,30 @@
+const data = {
+    id: 6897633067145,
+    title: "The Petit Blazer",
+    handle: "blazer-petit-double-jersey-steel",
+    image: "https://cdn.shopify.com/s/files/1/0568/3983/1689/products/BC55110223_STEEL_B.jpg?v=1659691689",
+    sku: "BC55110223/1740/34",
+    name: "Steel",
+    type: "one_color",
+    data: "#494952"
+  };
+
+
+
+
+let e = document.querySelectorAll(".swatchProductColor");
+
+e.addEventListener('mouseover', () => {
+
+    //id ophalen van desbetreffende object waar je overheen hovert
+
+    // forEach om door de array heen te loopen om zo een id op te halen
+     data.forEach()
+     
+    // de bijbehorende image ophalen
+    // de bijbehorende image tonen
+    
+
+
+    })
+
